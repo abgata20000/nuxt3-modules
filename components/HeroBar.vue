b6fa4e3c0a194c8b91fc4fcd55fd28c5f8608f56
rintarotawara <rintarou.54@gmail.com>
2022-12-01 11:04:35 +0900

    HeroBarに前へと次へを追加

--- components/HeroBar.vue
+++ components/HeroBar.vue
@@ -3,18 +3,30 @@ interface Props {
   title: string
   back?: string
   backLabel?: string
+  next?: string
+  prev?: string
 }
-const props = withDefaults(defineProps<Props>(), { back: '', backLabel: '戻る' })
+const props = withDefaults(defineProps<Props>(), { back: '', backLabel: '戻る', next: '', prev: '' })
 </script>
 <template>
   <section class="hero is-hero-bar">
     <div class="hero-body">
       <div class="level">
         <div class="level-left">
+          <div class="level-item" v-if="props.prev !== ''">
+            <nuxt-link :to="props.prev">
+              <i class="fas fa-arrow-alt-circle-left" />
+            </nuxt-link>
+          </div>
           <div class="level-item">
             <h1 class="title">
               {{ props.title }}
             </h1>
+          </div>
+          <div class="level-item" v-if="props.next !== ''">
+            <nuxt-link :to="props.next">
+              <i class="fas fa-arrow-alt-circle-right" />
+            </nuxt-link>
           </div>
         </div>
         <div v-if="props.back !== ''" class="level-right">
